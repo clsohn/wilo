fb88536cdec83a50b4f3e69e1be307769f1f9e7a
Christine <christinesohn01@gmail.com>
2017-05-08 19:12:55 -0400

    location and routes glitches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
 before_action :authorize_user, only: [:edit, :update, :show]
 
-  def index
-    @users = User.all
-    userlocation = UsersLocation.last
-    @location = Location.find(userlocation.location_id)
-    @weather = Weather.new(@temp, @condition, @wspd, @humidity, @snow, @location)
-    @message = Message.new(@weather)
-  end
+  def index; end
 
   def show
     @user = User.find(params[:id])
-    userlocation = UsersLocation.last
-    @location = Location.find(userlocation.location_id)
+    user_locations = UsersLocation.where(user: @user)
+    ul = user_locations.pluck(:location_id)
+    @location = Location.where(id: ul).last
+  end
+
+  def new
+    @user = current_user
+    user_locations = UsersLocation.where(user: @user)
+    ul = user_locations.pluck(:location_id)
+    @location = Location.where(id: ul).last
+    @weather = Weather.new(@temp, @condition, @wspd, @humidity, @snow, @location)
+    @message = Message.new(@weather)
   end
 
   def authorize_user
